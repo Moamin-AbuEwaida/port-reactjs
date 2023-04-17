d53c4a6b4f5ef6ad01252ddab95498e4605ce863
Moamin <moamin.salamah@gmail.com>
2023-04-17 10:46:13 +0200

    header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import About from "./components/About";
+import Header from "./components/Header";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
+      <Header />
       <Nav />
       <About />
       <div className='h-[4000px]'></div>
