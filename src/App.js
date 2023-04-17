990367d9ace4ad485d58e4bc9b08d3445d880cfa
Moamin <moamin.salamah@gmail.com>
2023-04-17 12:07:47 +0200

    banner component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import Banner from "./components/Banner";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
       <Header />
+      <Banner />
       <Nav />
       <About />
       <div className='h-[4000px]'></div>
