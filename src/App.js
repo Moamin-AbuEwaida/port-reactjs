81cb3416859826dff8d4b72bc473eb3e98755031
Moamin <moamin.salamah@gmail.com>
2023-04-17 10:26:34 +0200

    adding contact link to nav component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import About from "./components/About";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
       <Nav />
+      <About />
       <div className='h-[4000px]'></div>
     </div>
   );
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <div>About</div>
+    </div>
+  );
+};
+
+export default About;
