3b6797f3e64361a549b2bcbc483896b31878ac97
Moamin <moamin.salamah@gmail.com>
2023-04-17 10:01:50 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="bg-site bg-no-repeat bg-cover overflow-hidden">
+      <h1>Hello world</h1>
+      <div className='h-[4000px]'></div>
+    </div>
+  );
+}
+
+export default App;
